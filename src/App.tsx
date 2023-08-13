34b7ec8f70c87ff2913e36fc139d62d0408d7550
MMinauf <minaufmario.og@gmail.com>
2023-08-13 16:10:36 +0200

    react_query_v1

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,30 @@ import { Pokemon } from './components/types/Pokemon';
 import Pokedex from './components/Pokedex/Pokedex';
 import { fetchPokemonList } from './api/fetchPokemonList';
 import PokemonModal from './components/PokemonModal/PokemonModal';
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+  UseQueryResult,
+  QueriesResults,
+} from 'react-query';
 
 function App() {
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  //const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
   const [modal, setModal] = useState<boolean>(false);
   const [pokemonData, setPokemonData] = useState<Pokemon>();
 
-  useEffect(() => {
+  /*useEffect(() => {
     (async () => {
       setPokemonList(await fetchPokemonList(1));
     })();
   }, []);
+*/
+  const { isLoading, data: pokemonList } = useQuery({
+    queryKey: ['pokemonList'],
+    queryFn: async () => fetchPokemonList(1),
+  });
 
   return (
     <>
@@ -30,11 +43,13 @@ function App() {
         setModal={setModal}
       ></HeroSection>{' '}
       <FilterSection></FilterSection>
-      <Pokedex
-        pokemonList={pokemonList}
-        setModal={setModal}
-        setPokemonData={setPokemonData}
-      ></Pokedex>
+      {pokemonList && (
+        <Pokedex
+          pokemonList={pokemonList}
+          setModal={setModal}
+          setPokemonData={setPokemonData}
+        ></Pokedex>
+      )}
     </>
   );
 }
--- src/api/fetchPokemonList.ts
+++ src/api/fetchPokemonList.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { fetchPokemon } from './fetchPokemon';
+import { Pokemon } from '../components/types/Pokemon';
 
 export const baseAPI = axios.create({
   baseURL: 'https://pokeapi.co/api/v2',
 });
 
-export const fetchPokemonList = async (page: number) => {
+export const fetchPokemonList = async (page: number): Promise<Pokemon[]> => {
   const offset = 9 * (page - 1);
 
   const response = await baseAPI.get(`/pokemon?offset=${offset}&limit=9`);
